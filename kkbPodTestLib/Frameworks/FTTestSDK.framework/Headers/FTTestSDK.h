95340307c2ed3877d67a721160bc033dbe379479
付桃 <tfu@gaoxiaobang.com>
2020-06-18 16:32:25 +0800

    增加Framework

---
+++ kkbPodTestLib/Frameworks/FTTestSDK.framework/Headers/FTTestSDK.h
@@ -0,0 +1,20 @@
+//
+//  FTTestSDK.h
+//  FTTestSDK
+//
+//  Created by 西门桃桃 on 2020/6/18.
+//  Copyright © 2020 huikeyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <FTTestSDK/KKBTest.h>
+
+//! Project version number for FTTestSDK.
+FOUNDATION_EXPORT double FTTestSDKVersionNumber;
+
+//! Project version string for FTTestSDK.
+FOUNDATION_EXPORT const unsigned char FTTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTTestSDK/PublicHeader.h>
+
+
---
+++ kkbPodTestLib/Frameworks/FTTestSDK.framework/Headers/KKBTest.h
@@ -0,0 +1,19 @@
+//
+//  KKBTest.h
+//  ximentaotao
+//
+//  Created by 西门桃桃 on 2020/6/16.
+//  Copyright © 2020 huikeyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KKBTest : NSObject
+
+- (void)output:(NSString *)string;
+
+@end
+
+NS_ASSUME_NONNULL_END
